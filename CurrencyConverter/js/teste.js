7d3a11f990228289603ddb632f6bb0baaaf5bbc4
Fábio Pinto <pintofabio100@gmail.com>
2022-03-12 02:28:29 -0300

    Parsing currency json into menu items

--- CurrencyConverter/js/teste.js
+++ CurrencyConverter/js/teste.js
@@ -1,9 +1,33 @@
 fetch('../currencies.json')
-.then(response => {
-   return response.json();
-})
-.then(jsondata => console.log(jsondata));
+   .then(response => {
+      return response.json();
+   })
+   .then(jsondata => parser(jsondata));
 
-function parse(jsonData){
-    
+function parser(jsonData) {
+
+   const currencies = jsonData['currencies'];
+
+   var list = []
+
+   for (var key in currencies) {
+      var value = currencies[key];
+
+      var s = key + " - " + value;
+
+      list.push(
+         document.getElementById("currency-list").innerHTML +
+         '<li><a class="dropdown-item py-0 my-0" onclick="changeButtonText(' + "\'" + s +  "\'" + ');">' +
+         key + " - " + value +
+         '</a></li>'
+      );
+
+   }
+   document.getElementById("currency-list").innerHTML = list;
+
+}
+
+function changeButtonText(text) {
+   console.log(text);
+   document.getElementById("button1").innerHTML = text;
 }
