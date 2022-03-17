c7c95265ba62a4d4afa9752971ba271a83aaa40a
Fábio Pinto <pintofabio100@gmail.com>
2022-03-17 19:34:51 -0300

    .

--- WeatherApp/js/GetWeather.js
+++ WeatherApp/js/GetWeather.js
@@ -1,26 +1,33 @@
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
+function test() {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
 
-fetch("https://api.getgeoapi.com/v2/ip/177.188.140.52?api_key=f4d469f5612fbc93de8286b5517c7fd5b9531831&format=json&filter=city&language=en", requestOptions)
+    let uriIP = "https://api.ipify.org/?format=json"
+
+    var ip;
+    fetch(uriIP, requestOptions)
+        .then(response => response.json())
+        .then(result => )
+        .catch(error => console.log(error))
+
+}
+
+console.log(test());
+/* 
+
+uri = "https://api.getgeoapi.com/v2/ip/177.188.140.52?api_key=f4d469f5612fbc93de8286b5517c7fd5b9531831&format=json&filter=city&language=en";
+
+fetch(uri, requestOptions)
     .then(response => response.text())
-    .then(result => console.log(result))
+    .then(result => parse(result))
     .catch(error => console.log('error', error));
 
-
-fetch('../location.json').then(response => {
-    return response.json();
-}).then(data => {
-    parse(data);
-}).catch(err => {
-    alert('Something went wrong error: \n' + err);
-});
-
-
 function parse(jsondata) {
+    jsondata = JSON.parse(jsondata);
     let longitude = jsondata['location']['longitude'];
     let latitude = jsondata['location']['latitude'];
 
     console.log(longitude + " " + latitude);
-}
+} */
