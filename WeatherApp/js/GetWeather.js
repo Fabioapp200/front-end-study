2159673c38aab9fc56412fc14287165e4ed0be53
Fábio Pinto <pintofabio100@gmail.com>
2022-03-18 22:17:53 -0300

    NEW IDEA

--- WeatherApp/js/GetWeather.js
+++ WeatherApp/js/GetWeather.js
@@ -1,20 +1,14 @@
-function test() {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+var ip = 1;
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+//const IPUri = "https://api.ipify.org/?format=json";
 
-    let uriIP = "https://api.ipify.org/?format=json"
 
-    var ip;
-    fetch(uriIP, requestOptions)
-        .then(response => response.json())
-        .then(result => )
-        .catch(error => console.log(error))
+//FETCH 1 > USE DATA TO FETCH 2 > USE DATA TO FETCH 3 > USE DATA TO BUILD PAGE 
 
-}
-
-console.log(test());
+    
 /* 
 
 uri = "https://api.getgeoapi.com/v2/ip/177.188.140.52?api_key=f4d469f5612fbc93de8286b5517c7fd5b9531831&format=json&filter=city&language=en";
