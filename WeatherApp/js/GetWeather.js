1f1f4e8d9b13834f0f69e8735a624f98dc73e39b
Fábio Pinto <pintofabio100@gmail.com>
2022-03-19 01:50:50 -0300

    getting data is working

--- WeatherApp/js/GetWeather.js
+++ WeatherApp/js/GetWeather.js
@@ -1,27 +1,32 @@
-var ip = 1;
-var requestOptions = {
+const requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
-//const IPUri = "https://api.ipify.org/?format=json";
+const IPUri = "https://api.ipify.org/?format=json";
 
+function getValues(data) {
+    console.log(JSON.parse(data));
+}
 
-//FETCH 1 > USE DATA TO FETCH 2 > USE DATA TO FETCH 3 > USE DATA TO BUILD PAGE 
-
-    
-/* 
-
-uri = "https://api.getgeoapi.com/v2/ip/177.188.140.52?api_key=f4d469f5612fbc93de8286b5517c7fd5b9531831&format=json&filter=city&language=en";
-
-fetch(uri, requestOptions)
-    .then(response => response.text())
-    .then(result => parse(result))
-    .catch(error => console.log('error', error));
-
-function parse(jsondata) {
-    jsondata = JSON.parse(jsondata);
-    let longitude = jsondata['location']['longitude'];
-    let latitude = jsondata['location']['latitude'];
-
-    console.log(longitude + " " + latitude);
-} */
+fetch(IPUri, requestOptions)
+    .then(resp => resp.text())
+    .then(function (res) {
+        const ip = JSON.parse(res)['ip'];
+        const api_key = "f4d469f5612fbc93de8286b5517c7fd5b9531831";
+        const geoURL = `https://api.getgeoapi.com/v2/ip/${ip}?api_key=${api_key}`;
+        fetch(geoURL, requestOptions)
+            .then(resp => resp.text())
+            .then(function (res) {
+                jsondata = JSON.parse(res);
+                const api_key = "a7b6c24e8b4e46c984193533bc467d14";
+                const lon = jsondata['location']['longitude'];
+                const lat = jsondata['location']['latitude'];
+                const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
+                fetch(url, requestOptions)
+                    .then(resp => resp.text())
+                    .then(getValues(res))
+                    .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
